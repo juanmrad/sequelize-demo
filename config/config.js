480e4ef419d5354622ab88b6732f4fded6f40308
Juan Mrad <juansmrad@gmail.com>
2020-09-19 12:47:02 -0500

    temp fix for heroku connection locally

--- config/config.js
+++ config/config.js
@@ -7,6 +7,15 @@ module.exports = {
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
+    // for heroku uncomment this:
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false,
+    //   },
+    //   keepAlive: true,        
+    // },      
+    // ssl: true
   },
   "production": {
     "username": process.env.DB_USERNAME,
