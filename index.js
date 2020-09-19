f956eb0405642d43a810d5443df8648813c27e82
Juan Mrad <juansmrad@gmail.com>
2020-09-19 12:28:42 -0500

    add encryption to password. Add middleware and sign-up route

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const session = require('express-session');
-const bkdf2 = require('pbkdf2');
+const pbkdf2 = require('pbkdf2');
+const models = require('./models');
 
 require('dotenv').config();
 
@@ -15,9 +16,31 @@ app.use(session({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+function encryptPassword(req, res, next) {
+  if(req.body.password) {
+    var key = pbkdf2.pbkdf2Sync(
+      req.body.password, process.env.PASSWORD_SALT, 3600, 256, 'sha256'
+    );
+  
+    var hash = key.toString('hex');
+    req.body.password = hash;
+    next();
+  } else {
+    res.send('please add password');
+  }
+}
+
 app.get('/', function (req, res){
   res.send(process.env.HELLO_MESSAGE);
 });
+
+app.post('/sign-up', encryptPassword, function (req,res) {
+  models.user.create({ username: req.body.username, password: req.body.password })
+  .then(function (user) {
+    console.log("new user created: ", user);
+    res.send(user);
+  })
+})
 
 app.listen(process.env.PORT, function (){
   console.log( `My API is listening to port ${process.env.PORT}`)
